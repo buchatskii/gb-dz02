d4a769163554c0715966419d9d2ce14fe8016298
Artur <buch16@bk.ru>
2022-08-10 10:52:01 +0300

    ДЗ решил, финальный пуш

--- Program.cs
+++ Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+// Console.Write("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int sec = number / 10;
+// int sec2 = sec % 10;
+// if (number >= 100 && number <= 999)
+// {
+    
+//      Console.WriteLine(sec2);
+// }
+// else if (number <= 0)
+// {
+//      Console.WriteLine(-sec2);
+// }
+// else
+// {
+//      Console.WriteLine("Введено не трехзначное число");
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// Console.Write("Введите число: ");
+// string str = Console.ReadLine();
+
+// char[] ch = str.ToCharArray();
+// if (ch.Length <= 2)
+// {
+//      Console.Write("третьей цифры нет");
+// }
+// else
+// {
+//      Console.Write(ch[2]);
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Write("Работаем? ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number >= 1 && number <= 5)
+// {
+//     Console.WriteLine("Да");
+// }
+// else if (number <= 0 || number >= 8)
+// {
+//     Console.WriteLine("Не в нашей вселенной)");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// } 
+
+
